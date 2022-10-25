6d6aad6218bf2a1fa8c15df689a389b6c1beab41
dickisujadi <dicki.sujadi@eaciit.com>
2022-10-25 11:05:13 +0700

    debugging preparation

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const message = 'Hello World';
+const second_message = 'My Name is Dicki';
+console.log(`${message} ${second_message}`);
+//# sourceMappingURL=index.js.map
--- index.ts
+++
@@ -1,4 +0,0 @@
-const message : String = 'Hello World'
-const second_message : String = 'My Name is Dicki'
-
-console.log(`${message} ${second_message}`)
