11a1b97b33746e9f408af247b2bb9f64f4e5da56
dickisujadi <dicki.sujadi@eaciit.com>
2022-10-27 10:47:03 +0700

    enum optimize code

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,5 @@
 "use strict";
-var Size;
-(function (Size) {
-    Size[Size["Small"] = 1] = "Small";
-    Size[Size["Medium"] = 2] = "Medium";
-    Size[Size["Large"] = 3] = "Large";
-})(Size || (Size = {}));
 ;
-let mySize = Size.Medium;
+let mySize = 2;
 console.log(mySize);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@
 // const large = 3;
 
 // PascalCase
-enum Size { Small = 1, Medium, Large };
+const enum Size { Small = 1, Medium, Large };
 let mySize: Size = Size.Medium;
 console.log(mySize);
