5f352c52fcdc666bd3c658977218cdab3bd92477
dickisujadi <dicki.sujadi@eaciit.com>
2022-10-27 10:46:11 +0700

    enum

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-let age : number = 20
+// const small = 1;
+// const medium = 2;
+// const large = 3;
 
-if(age < 50)
-    age += 10
-
-console.log(age);
+// PascalCase
+enum Size { Small = 1, Medium, Large };
+let mySize: Size = Size.Medium;
+console.log(mySize);
